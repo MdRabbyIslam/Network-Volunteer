0ff1f3c2e3ec6aed899eb18d6c29dbd9f763ff4a
Rabby-sopno <sopno01737@gmail.com>
2020-10-13 03:53:15 +0600

    adevent.js updated

--- src/Components/Adevent/AdEvent.js
+++ src/Components/Adevent/AdEvent.js
@@ -1,20 +1,56 @@
 import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./AdEvent.css";
 
 const AdEvent = () => {
+  const [result, setResult] = useState({
+    eventTitle: "",
+    discription: "",
+    eventDate: new Date().toLocaleDateString(),
+    banner: "",
+  });
+
+  const history = useHistory();
+
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    const name = e.target.name;
+    const value = e.target.value;
+    const addInfo = { ...result };
+    addInfo[name] = value;
+    setResult(addInfo);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    const { eventTitle, discription, banner, eventDate } = result;
+    const finalResult = { eventTitle, eventDate, discription, banner };
+    fetch("https://frozen-anchorage-84492.herokuapp.com/addevents", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(finalResult),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          history.push("/");
+        }
+      });
+  };
   return (
     <div>
-      <form
-        className="event-form"
-        action="http://localhost:5000/addevents"
-        method="POST"
-      >
+      <form className="event-form" onSubmit={handleOnSubmit}>
         <label className="item-1" htmlFor="event-title">
           Event Title : <br />
           <input
             type="text"
             name="eventTitle"
             id="event-title"
+            value={result.title}
+            onChange={handleOnChange}
             placeholder="Enter Title"
           />
         </label>
@@ -23,17 +59,32 @@ const AdEvent = () => {
           <textarea
             name="discription"
             id="discription"
+            value={result.discription}
+            onChange={handleOnChange}
             cols="30"
             rows="10"
           ></textarea>
         </label>
         <label className="item-3" htmlFor="event-date">
           Event Date: <br />
-          <input type="date" name="eventDate" id="event-date" />
+          <input
+            type="date"
+            name="eventDate"
+            value={result.date}
+            onChange={handleOnChange}
+            id="event-date"
+          />
         </label>
         <label className="item-4" htmlFor="banner">
           Banner: <br />
-          <input type="text" name="banner" id="banner" placeholder="banner" />
+          <input
+            type="file"
+            name="banner"
+            value={result.banner}
+            onChange={handleOnChange}
+            id="banner"
+            placeholder="banner"
+          />
         </label>
         <label className="item-5" htmlFor="">
           <input type="submit" value="submit" />
